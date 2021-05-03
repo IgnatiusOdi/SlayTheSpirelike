4bfdcb0f83ee102466fdc2f828cc8cc280a6202e
CBEngineer <CBEalexanderkevin@gmail.com>
2021-05-03 20:55:25 +0700

    team name

--- src/com/SlayTheSpirelike/Main.java
+++ src/com/SlayTheSpirelike/Main.java
@@ -1,5 +1,13 @@
 package com.SlayTheSpirelike;
 
+/**
+ * Slay the Spirelike <br>
+ * Alexander Kevin <br>
+ * Ignatius Odi <br>
+ * Airlangga <BR>
+ * Abraham Arthur <br>
+ */
+
 public class Main {
 
     public static void main(String[] args) {
