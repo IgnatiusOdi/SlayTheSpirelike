755503b1887152f963baa074ad8033f7cc421920
AzhureRaven <abrahamarthurfendy@gmail.com>
2021-05-12 09:02:35 +0700

    Potion Test Bekerja Abraham Arthur Fendy

--- src/com/SlayTheSpirelike/Main.java
+++ src/com/SlayTheSpirelike/Main.java
@@ -11,7 +11,7 @@ package com.SlayTheSpirelike;
 public class Main {
 
     public static void main(String[] args) {
-        //Body body = new Body(1060, 600);
+        Body body = new Body(1060, 600);
 //        Kapal k = new Aircraft("nama");
 //        Potion p = new AttackPotion();
 //        p.activate(k);
