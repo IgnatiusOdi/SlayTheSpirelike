6c90321f9841e65bc5177d242bb99b1777877447
AzhureRaven <abrahamarthurfendy@gmail.com>
2021-05-19 10:19:32 +0700

    Heli Summon sprite

--- src/com/SlayTheSpirelike/HeliSummon.java
+++ src/com/SlayTheSpirelike/HeliSummon.java
@@ -2,6 +2,6 @@ package com.SlayTheSpirelike;
 
 public class HeliSummon extends Summon{
     public HeliSummon() {
-        super("Heli", 3, 3, 1);
+        super("Heli", 3, 3, 1,"resources/summonPotion");
     }
 }
--- src/com/SlayTheSpirelike/Summon.java
+++ src/com/SlayTheSpirelike/Summon.java
@@ -1,17 +1,34 @@
 package com.SlayTheSpirelike;
 
+import javax.swing.*;
+
 public abstract class Summon {
     //jika ada summon, enemy akan attack summon yang terbaru
     //summon attack setelah player selesai attack, attack random/musuh terdepan
 
     protected String nama;
     protected int health, maxhealth, attack;
+    protected JLabel summon;
+    protected JPanel panel;
 
-    public Summon(String nama, int health, int maxhealth, int attack) {
+    JFrame frame;
+
+    public Summon(String nama, int health, int maxhealth, int attack,String image) {
         this.nama = nama;
         this.health = health;
         this.maxhealth = maxhealth;
         this.attack = attack;
+        this.summon.setIcon(new ImageIcon(image));
+        this.panel = new JPanel();
+        this.panel.add(summon);
+        this.panel.setSize(50,50);
+        this.panel.setOpaque(false);
+        //uncomment to look
+//        frame = new JFrame();
+//        frame.setBackground(Color.BLUE);
+//        frame.add(panel);
+//        frame.pack();
+//        frame.setVisible(true);
     }
 
     public String getNama() {
